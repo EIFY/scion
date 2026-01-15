ecf7e43aeb81859375297a2e6d7f605ea22568cb
EIFY <chuanchih@gmail.com>
2026-01-14 22:07:42 -0800

    no_grad() helps but doesn't solve the root cause
    $ torchrun train_gpt_scion_corrected.py
    using device: cuda:0
    ======== Arguments ========
    Hyperparameters(input_bin='/data/fineweb10B/fineweb_train_*.bin', input_val_bin='/data/fineweb10B/fineweb_val_*.bin', batch_size=16, device_batch_size=16, sequence_length=1024, num_iterations=3, learning_rate=0.01220703125, sign_lr=0.732421875, warmup_iters=1, weight_decay=0.000244140625, grad_clip_norm=1000000.0, val_loss_every=125, save_every=0, n_layer=12, n_head=6, n_embd=768, momentum=0.1)
    ===========================
    Training DataLoader: total number of tokens: 100000000 across 1 files
    Validation DataLoader: total number of tokens: 100000000 across 1 files
    Just model: 1416 MiB
    After attn: 1523 MiB
    After mlp: 1532 MiB
    After attn: 1534 MiB
    After mlp: 1543 MiB
    After attn: 1545 MiB
    After mlp: 1554 MiB
    After attn: 1556 MiB
    After mlp: 1565 MiB
    After attn: 1567 MiB
    After mlp: 1576 MiB
    After attn: 1578 MiB
    After mlp: 1587 MiB
    After attn: 1590 MiB
    After mlp: 1599 MiB
    After attn: 1601 MiB
    After mlp: 1610 MiB
    After attn: 1613 MiB
    After mlp: 1622 MiB
    After attn: 1624 MiB
    After mlp: 1633 MiB
    After attn: 1635 MiB
    After mlp: 1644 MiB
    After attn: 1646 MiB
    After mlp: 1655 MiB
    step:0/3 val_loss:15.9915 train_time:129ms step_avg:nanms
    After attn: 4938 MiB
    After mlp: 5403 MiB
    After attn: 5722 MiB
    After mlp: 6187 MiB
    After attn: 6506 MiB
    After mlp: 6971 MiB
    After attn: 7290 MiB
    After mlp: 7755 MiB
    After attn: 8074 MiB
    After mlp: 8539 MiB
    After attn: 8858 MiB
    After mlp: 9323 MiB
    After attn: 9642 MiB
    After mlp: 10107 MiB
    After attn: 10426 MiB
    After mlp: 10891 MiB
    After attn: 11210 MiB
    After mlp: 11675 MiB
    After attn: 11994 MiB
    After mlp: 12459 MiB
    After attn: 12778 MiB
    After mlp: 13243 MiB
    After attn: 13562 MiB
    After mlp: 14027 MiB
    [rank0]: Traceback (most recent call last): (OOM)

--- examples/modded-nanogpt/train_gpt_scion_corrected.py
+++ examples/modded-nanogpt/train_gpt_scion_corrected.py
@@ -103,7 +103,9 @@ class Block(nn.Module):
 
     def forward(self, x):
         x = x + self.attn(F.rms_norm(x, (x.size(-1),)))
+        print(f"After attn: {torch.cuda.memory_allocated() // 1024 // 1024} MiB")
         x = x + self.mlp(F.rms_norm(x, (x.size(-1),)))
+        print(f"After mlp: {torch.cuda.memory_allocated() // 1024 // 1024} MiB")
         return x
 
 # -----------------------------------------------------------------------------
@@ -224,10 +226,10 @@ class Hyperparameters:
     input_bin : str = '/data/fineweb10B/fineweb_train_*.bin' # input .bin to train on
     input_val_bin : str = '/data/fineweb10B/fineweb_val_*.bin' # input .bin to eval validation loss on
     # optimization hyperparams
-    batch_size : int = 8 # batch size, in sequences, across all devices
-    device_batch_size : int = 8 # batch size, in sequences, per device
+    batch_size : int = 16 # batch size, in sequences, across all devices
+    device_batch_size : int = 16 # batch size, in sequences, per device
     sequence_length : int = 1024 # sequence length, in tokens
-    num_iterations : int = 0 # number of iterations to run
+    num_iterations : int = 3 # number of iterations to run
     learning_rate : float = 2 ** -12 * 50
     corrected = True
     sign_lr : float = 2 ** -12 * 3000
@@ -292,7 +294,7 @@ def main():
     model = model.cuda()
     if hasattr(config, "coordinate_descent_tuning"):
         config.coordinate_descent_tuning = True # suggested by @Chillee
-    model = torch.compile(model)
+    # model = torch.compile(model)
     # here we wrap model into DDP container
     model = DDP(model, device_ids=[ddp_local_rank])
     raw_model = model.module # always contains the "raw" unwrapped model
@@ -388,6 +390,10 @@ def main():
     t0 = time.time()
     # begin training
     train_loader.reset()
+
+    print(f"Just model: {torch.cuda.memory_allocated() // 1024 // 1024} MiB")
+    val_steps = 1
+
     for step in range(args.num_iterations + 1):
         last_step = (step == args.num_iterations)
         # This effectively ignores timing first 10 steps, which are slower for weird reasons.
@@ -409,7 +415,7 @@ def main():
             val_loss = 0.0
             for _ in range(val_steps):
                 x_val, y_val = val_loader.next_batch()
-                with ctx: # of course, we'd like to use no_grad() here too, but that creates a torch.compile error for some reason
+                with ctx as _, torch.no_grad() as _:
                     logits = model(x_val)
                     loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y_val.view(-1), ignore_index=-1)
                     val_loss += loss.detach()
@@ -451,6 +457,7 @@ def main():
                 optimizer1.sync_state()
             optimizer1.remove_unused_keys()
             torch.cuda.empty_cache()
+            print(f"After empty_cache(): {torch.cuda.memory_allocated() // 1024 // 1024} MiB")
 
         # bit confusing: we want to make sure to eval on 0th iteration
         # but also after the very last iteration. so we loop for step <= num_iterations
