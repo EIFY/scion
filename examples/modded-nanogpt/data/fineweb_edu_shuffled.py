7f56308be38876ee220de2ae38bd0eba92be24fa
EIFY <chuanchih@gmail.com>
2026-06-19 20:22:39 -0700

    use HuggingFaceFW/(fineweb|fineweb_edu)_100BT-shuffled
    Their existence strongly indicates that the plain versions aren't
    shuffled.

---
+++ examples/modded-nanogpt/data/fineweb_edu_shuffled.py
@@ -0,0 +1,142 @@
+"""
+FineWeb-edu dataset (for srs pretraining)
+https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu
+
+example doc to highlight the structure of the dataset:
+{
+  "text": "Posted by mattsmith on 20th April 2012\nStraight from...",
+  "id": "<urn:uuid:d853d453-196e-4488-a411-efc2b26c40d2>",
+  "dump": "CC-MAIN-2013-20",
+  "url": "http://nleastchatter.com/philliesphandom/tag/freddy-galvis/",
+  "date": "2013-05-18T07:24:47Z",
+  "file_path": "s3://commoncrawl/long.../path.../file.gz",
+  "language": "en",
+  "language_score": 0.9185474514961243,
+  "token_count": 594
+}
+"""
+import functools
+import os
+import argparse
+import multiprocessing as mp
+import numpy as np
+import tiktoken
+# from huggingface_hub import snapshot_download
+from datasets import Dataset, load_dataset
+from tqdm import tqdm
+import argparse
+import numpy as np
+def write_datafile(filename, toks):
+    """ 
+    Saves token data as a .bin file, for reading in C.
+    - First comes a header with 256 int32s
+    - The tokens follow, each as a uint16
+    """
+    assert len(toks) < 2**31, "token count too large" # ~2.1B tokens
+    # construct the header
+    header = np.zeros(256, dtype=np.int32)
+    header[0] = 20240520 # magic
+    header[1] = 1 # version
+    header[2] = len(toks) # number of tokens after the 256*4 bytes of header (each 2 bytes as uint16)
+    # construct the tokens numpy array, if not already
+    if not isinstance(toks, np.ndarray) or not toks.dtype == np.uint16:
+        # validate that no token exceeds a uint16
+        maxtok = 2**16
+        assert all(0 <= t < maxtok for t in toks), "token dictionary too large for uint16"
+        toks_np = np.array(toks, dtype=np.uint16)
+    else:
+        toks_np = toks
+    # write to file
+    print(f"writing {len(toks):,} tokens to {filename}")
+    with open(filename, "wb") as f:
+        f.write(header.tobytes())
+        f.write(toks_np.tobytes())
+# ------------------------------------------
+
+BATCH_SIZE = 8*64*1024 # Intended BS in terms of number of tokens
+
+parser = argparse.ArgumentParser(description="FineWeb-Edu shuffled preprocessing")
+parser.add_argument("-v", "--version", type=str, default="fineweb_edu", help="Which version to use: fineweb_edu or fineweb")
+parser.add_argument("-s", "--train_shard_size", type=int, default=200 * BATCH_SIZE + 1, help="Size of each train shard in tokens")
+parser.add_argument("--val_shard_size", type=int, default=20 * BATCH_SIZE + 1, help="Size of each val shard in tokens")
+parser.add_argument("--test", action='store_true', help="Test with 'The Fox and the Grapes'")
+
+
+def tokenize_with(doc, enc, eot):
+    # tokenizes a single document and returns a numpy array of uint16 tokens
+    tokens = [eot] # the special <|endoftext|> token delimits all documents
+    tokens.extend(enc.encode_ordinary(doc["text"]))
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16)
+    return tokens_np_uint16
+
+
+def main():
+    args = parser.parse_args()
+    if args.test:
+        local_dir = "test"
+        DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+        os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+        def gen():
+            yield {'text': 'A hungry Fox saw some fine bunches of Grapes hanging from a vine that was trained along a high trellis, and did his best to reach them by jumping as high as he could into the air. But it was all in vain, for they were just out of reach: so he gave up trying, and walked away with an air of dignity and unconcern, remarking, "I thought those Grapes were ripe, but I see now they are quite sour."'}
+        fw = Dataset.from_generator(gen)
+    else:
+        # FineWeb-Edu has a few possible subsamples available
+        assert args.version in ["fineweb_edu", "fineweb"], "version must be either fineweb_edu or fineweb"
+        local_dir = remote_name = f"{args.version}_100BT-shuffled"
+
+        # create the cache the local directory if it doesn't exist yet
+        DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+        os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+        # download the dataset
+        fw = load_dataset("HuggingFaceFW/" + remote_name, split="train")
+    # init the tokenizer
+    enc = tiktoken.get_encoding("gpt2")
+    eot = enc._special_tokens['<|endoftext|>'] # end of text token
+    tokenize = functools.partial(tokenize_with, enc=enc, eot=eot)
+
+    # tokenize all documents and write output shards, each of {val|train}_shard_size tokens (last shard has remainder)
+    nprocs = max(1, os.cpu_count() - 2) # don't hog the entire system
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        split = "val"
+        shard_size = args.val_shard_size
+        # preallocate buffer to hold current shard
+        all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+        for tokens in pool.imap(tokenize, fw, chunksize=16):
+
+            while token_count + len(tokens) >= shard_size:
+                # write the current shard and start a new one
+                filename = os.path.join(DATA_CACHE_DIR, f"fineweb_edu_{split}_{shard_index:06d}.bin")
+                # split the document into whatever fits in this shard; the remainder goes to next one
+                remainder = shard_size - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+                write_datafile(filename, all_tokens_np)
+                if not shard_index:
+                    split = "train"
+                    shard_size = args.train_shard_size
+                    all_tokens_np = np.empty((args.train_shard_size,), dtype=np.uint16)
+                shard_index += 1
+                token_count = 0
+                progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+                tokens = tokens[remainder:]
+
+            # simply append tokens to current shard
+            all_tokens_np[token_count:token_count+len(tokens)] = tokens
+            token_count += len(tokens)
+            # update progress bar
+            progress_bar.update(len(tokens))
+
+        # write any remaining tokens as the last shard
+        if token_count != 0:
+            filename = os.path.join(DATA_CACHE_DIR, f"fineweb_edu_{split}_{shard_index:06d}.bin")
+            write_datafile(filename, all_tokens_np[:token_count])
+
+
+if __name__ == '__main__':
+    main()
